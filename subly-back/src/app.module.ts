87b95a0b914ec70e7fae824d48d08aa89dab5f19
Tayyyyylor <bryan.h.pro@gmail.com>
2025-03-08 00:08:39 +0100

    add .en, link bdd with back, add first entity

--- subly-back/src/app.module.ts
+++ subly-back/src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: true, // ⚠️ à désactiver en prod
+    }),
+  ],
+})
+export class AppModule implements OnModuleInit {
+  onModuleInit() {
+    console.log('✅ Database connection established successfully!');
+  }
+}
