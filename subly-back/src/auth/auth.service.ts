9f2d67c1a353a5216d1e3761b84dfdc24f8db00f
Tayyyyylor <bryan.h.pro@gmail.com>
2025-03-15 10:34:12 +0100

    add bcrypt and hash for password

--- subly-back/src/auth/auth.service.ts
+++ subly-back/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -14,10 +15,13 @@ export class AuthService {
     pass: string,
   ): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(username);
-    console.log('user', user);
+    if (!user) {
+      throw new UnauthorizedException('Utilisateur non trouvé');
+    }
+    const isPasswordValid = await bcrypt.compare(pass, user.password);
     console.log('pass', pass);
-    if (user?.password !== pass) {
-      throw new UnauthorizedException();
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Mot de passe incorrect');
     }
     const payload = { sub: user.id, username: user.username };
 
