b703bfbe5b4c5cab50ea72fc3f25a2aff4b58cd8
Tayyyyylor <bryan.h.pro@gmail.com>
2025-04-24 18:20:23 +0200

    add icons on tabs and rearrange order

--- subly-front/app/(tabs)/_layout.tsx
+++ subly-front/app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { Redirect, Tabs } from 'expo-router';
 import { View, ActivityIndicator } from 'react-native';
 import { useAuth } from '@/context/useAuth';
 import '../../global.css';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   const { session, isLoading } = useAuth();
@@ -20,11 +21,28 @@ export default function TabLayout() {
 
   return (
     <Tabs
-      screenOptions={{ tabBarActiveTintColor: 'orange', headerShown: false }}
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: 'orange',
+        tabBarInactiveTintColor: 'gray',
+        headerShown: false,
+        tabBarIcon: ({ color, focused, size }) => {
+          let iconName: keyof typeof Ionicons.glyphMap = 'help';
+
+          if (route.name === 'index') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          } else if (route.name === 'stats') {
+            iconName = focused ? 'bar-chart' : 'bar-chart-outline';
+          }
+
+          return <Ionicons name={iconName} size={20} color={color} />;
+        },
+      })}
     >
       <Tabs.Screen name="index" options={{ title: 'Dashboard' }} />
-      <Tabs.Screen name="settings" options={{ title: 'Settings' }} />
       <Tabs.Screen name="stats" options={{ title: 'Stats' }} />
+      <Tabs.Screen name="settings" options={{ title: 'Settings' }} />
     </Tabs>
   );
 }
