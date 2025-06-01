2fe9d516ab050c51e17b6f17b074d55bced0f464
taylor <bryan.h.pro@gmail.com>
2025-06-01 11:36:25 +0200

    ajout de la suppression du user

--- subly-front/components/settings/Settings.tsx
+++ subly-front/components/settings/Settings.tsx
@@ -1,21 +1,48 @@
 import { useAuth } from '@/context/useAuth';
+import { deleteUser } from '@/services/authService';
+import { router } from 'expo-router';
 import React from 'react';
-import { Button, Pressable, SafeAreaView, Text } from 'react-native';
+import { Alert, Button, Pressable, SafeAreaView, Text } from 'react-native';
 
 const Settings = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+
+  const handleDelete = () => {
+    Alert.alert(
+      'Confirmation',
+      'Voulez-vous vraiment supprimer votre compte ?',
+      [
+        {
+          text: 'Annuler',
+          style: 'cancel',
+        },
+        {
+          text: 'Supprimer',
+          style: 'destructive',
+          onPress: async () => {
+            try {
+              await deleteUser(user?.id as string);
+              signOut();
+              Alert.alert('Succès', 'Utilisateur supprimé avec succès !');
+              router.push('/signin');
+            } catch (error) {
+              Alert.alert('Erreur', "Impossible de supprimer l'utilisateur.");
+            }
+          },
+        },
+      ],
+    );
+  };
 
   return (
     <SafeAreaView className="bg-[#121212] h-full">
       <Text className="text-white text-2xl font-bold text-center">Profil</Text>
 
       <Button title="Se déconnecter" color="red" onPress={signOut} />
-      <Pressable className="bg-orange-500 p-3 rounded-md ">
-        <Text className="text-white text-center">
-          Supprimer toutes les données
-        </Text>
-      </Pressable>
-      <Pressable className="bg-red-500 p-3 rounded-md mt-3">
+      <Pressable
+        className="bg-red-500 p-3 rounded-md mt-3"
+        onPress={handleDelete}
+      >
         <Text className="text-white text-center">Supprimer le compte</Text>
       </Pressable>
 
